673f6d5b6d0907f2a55cb07a054b07dcf69e8e0e
Aurick Anwar <aurick.anwar2260biga@gmail.com>
2026-07-23 17:00:57 -0400

    Update For Car Motion

--- scripts/diff_drive_publisher.py
+++ scripts/diff_drive_publisher.py
@@ -14,12 +14,28 @@ class DiffDrivePublisher(Node):
             10,
         )
         self.timer = self.create_timer(0.05, self.publish_command)
+        self.tick = 0  # your own counter (not a ROS command)
 
     def publish_command(self):
+        self.tick+=1
+
+        if self.tick <=40:
+            lin,ang = 0.5,0.0
+        elif self.tick <= 50:
+            lin,ang = 0.0,0.0
+        elif self.tick <= 70:
+            lin,ang = 0.0,0.5
+        elif self.tick <= 80:
+            lin,ang = 0.0,0.0
+        else:
+            self.tick = 0
+            lin,ang = 0.0,0.0
+        
+        
         command = TwistStamped()
         command.header.stamp = self.get_clock().now().to_msg()
-        command.twist.linear.x = 0.4
-        command.twist.angular.z = 0.4
+        command.twist.linear.x = lin
+        command.twist.angular.z = ang
         self.publisher.publish(command)
 
 
